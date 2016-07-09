4f02ac3bca9369e1d89ca0e1e35ce674078868d2
Christiano Milfont <cmilfont@gmail.com>
2016-07-09 16:20:18 -0300

    Component de Register

--- views/landing.jsx
+++ views/landing.jsx
@@ -5,21 +5,23 @@ export default class Landing extends React.Component {
 
   render() {
 
+    const username = (this.props.user)? this.props.user.username: '';
+
     return (
       <div className="mdl-layout mdl-js-layout">
         <header className="mdl-layout__header mdl-layout__header">
           <div className="mdl-layout__header-row">
             <span className="mdl-layout-title">Feedback</span>
             <div className="mdl-layout-spacer"></div>
-            <Menu />
+            <Menu user={this.props.user} />
           </div>
         </header>
         <div className="mdl-layout__drawer">
           <span className="mdl-layout-title">Feedback</span>
-          <Menu />
+          <Menu user={this.props.user} />
         </div>
         <main className="mdl-layout__content">
-        
+        {username}
         </main>
       </div>
     )
