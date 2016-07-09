016c484e18f84e7117926c69f1613f79808284ed
Christiano Milfont <cmilfont@gmail.com>
2016-07-09 15:07:37 -0300

    Estrutura inicial do controle de acesso

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import * as models from "./models";
+import Users from './routers/users';
 
 const app = express();
 require('./config/express.js')(express, app, __dirname);
+require('./config/passport.js')(express, app, __dirname, models);
+
+new Users(app, models);
+
 app.get('/*', function (req, res) {
     res.render('index', {user: req.user});
 });
