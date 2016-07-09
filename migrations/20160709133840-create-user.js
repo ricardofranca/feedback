8848b165b784b529925938caceacbbdf740594f8
Elvis <elvisdelima89@gmail.com>
2016-07-09 11:19:24 -0300

    changing user migration

---
+++ migrations/20160709133840-create-user.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+    up: function(queryInterface, Sequelize) {
+      return queryInterface.createTable('Users', {
+          id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER
+          },
+          username: {
+          type: Sequelize.STRING
+        }, hash: {
+          type: Sequelize.STRING
+        }, salt: {
+          type: Sequelize.STRING
+        }, email: {
+          type: Sequelize.STRING
+        }, activationKey: {
+          type: Sequelize.STRING
+        }, resetPasswordKey: {
+          type: Sequelize.STRING
+        }, verified: {
+          type: Sequelize.BOOLEAN 
+        }, createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        }, updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        }
+      });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Users');
+  }
+};
