eb9c37c6f94eeb84ccfb158e38a99acc1667e015
Christiano Milfont <cmilfont@gmail.com>
2016-07-16 17:49:16 -0300

    #4 atualizando o server-side

--- routers/invites.js
+++ routers/invites.js
@@ -5,12 +5,24 @@ export default class Invites {
     name: 'Chico',
     start: new Date,
     finish: new Date,
-    url: 'https://pbs.twimg.com/profile_images/85884918/barroso_200x200.JPG'
+    url: 'https://pbs.twimg.com/profile_images/85884918/barroso_200x200.JPG',
+    positive1: '',
+    positive2: '',
+    positive3: '',
+    negative1: '',
+    negative2: '',
+    negative3: ''
   },{
     id: 30,
     start: new Date,
     finish: new Date,
-    name: 'renoir'
+    name: 'renoir',
+    positive1: '',
+    positive2: '',
+    positive3: '',
+    negative1: '',
+    negative2: '',
+    negative3: ''
   }];
 
   getInvites(req, res) {
@@ -26,6 +38,20 @@ export default class Invites {
     res.send(invite);
   }
 
+  save(req, res) {
+    let invite = this.invites.reduce(function(a, b){
+      if(b.id == req.params.id) return b;
+      return a;
+    }, {});
+
+    Object.keys(req.body).forEach(function(key){
+      let value = req.body[key];
+      invite[key] = value;
+    })
+
+    res.send({});
+  }
+
   constructor(app, models) {
     this.app = app;
     this.models = models;
@@ -33,7 +59,7 @@ export default class Invites {
     app.get('/invites/:id.json', this.getInvite.bind(this));
     app.get('/invites.json', this.getInvites.bind(this));
 
-
+    app.put('/invites/:id.json', this.save.bind(this))
   }
 
 }
