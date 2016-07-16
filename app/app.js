16f1ece6498f404e9a5dd2a8bc43280e782aaa60
Christiano Milfont <cmilfont@gmail.com>
2016-07-16 13:26:57 -0300

    #4 Prototipando a tela de Invite

--- app/app.js
+++ app/app.js
@@ -1,12 +1,25 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import Invites from './invites/list';
 
 class App extends React.Component {
 
   render() {
+
+    const invites = [{
+      id: 23,
+      name: 'Chico',
+      url: 'https://pbs.twimg.com/profile_images/85884918/barroso_200x200.JPG'
+    },{
+      id: 30,
+      name: 'renoir'
+    }]
+
     return(
-      <div>
-        Olá
+      <div className="mdl-layout mdl-js-layout">
+        <main className="mdl-layout__content">
+          <Invites invites={invites} />
+        </main>
       </div>
     )
   }
