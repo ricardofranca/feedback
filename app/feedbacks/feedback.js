1a19461879263bc32b9441f3d0da96dae79bab33
Christiano Milfont <cmilfont@gmail.com>
2016-07-21 14:24:31 -0300

    Ajustando as duas colunas

---
+++ app/feedbacks/feedback.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import {Card, CardHeader, CardText} from 'material-ui/Card';
+import moment from 'moment';
+
+import Avatar from 'material-ui/Avatar';
+import Chip from 'material-ui/Chip';
+import Badge from 'material-ui/Badge';
+
+class Feedback extends Component {
+
+    mapInvited = invited => {
+
+      let disabled = (invited.feedbacks)? '': '-disabled';
+      const badgeStyle = { top: 10, right: 30 }
+      if(!invited.feedbacks) {
+        badgeStyle['backgroundColor'] = '#9E9E9E';
+        disabled = '-disabled';
+      }
+
+      return <Badge
+        badgeContent={invited.feedbacks}
+        secondary={invited.feedbacks}
+        badgeStyle={badgeStyle}
+      >
+        <Chip key={`chip-invited-${invited.id}`}
+              className={`invited-chip${disabled}`} >
+          <Avatar src={invited.url} />
+          {invited.name}
+        </Chip>
+      </Badge>;
+    }
+
+    render() {
+
+        const {feedback} = this.props;
+        const invited_list = feedback.invited.map(this.mapInvited);
+        const start = moment(feedback.start).format('MMMM Do YYYY');
+        const finish = moment(feedback.finish).format('MMMM Do YYYY');
+        const title = `${start} a ${finish}`;
+
+        return (
+            <Card key={`feedback-${feedback.id}`} className="feedback">
+                <CardHeader
+                title={title}
+                avatar={feedback.user.url}
+                />
+                <CardText className="feedback-invited-list">
+                  {invited_list}
+                </CardText>
+            </Card>
+        );
+    }
+}
+
+export default Feedback;
